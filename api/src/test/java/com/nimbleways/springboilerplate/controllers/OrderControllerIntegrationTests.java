9e532969d64db47960f18adeddf47c32d270cdf0
Ammar HOMSI <ahomsi@pca.ma>
2025-03-15 10:50:55 +0000

    fix: optimize imports in tests

--- api/src/test/java/com/nimbleways/springboilerplate/controllers/OrderControllerIntegrationTests.java
+++ api/src/test/java/com/nimbleways/springboilerplate/controllers/OrderControllerIntegrationTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.assertEquals;
@@ -30,6 +31,7 @@ import java.util.Set;
 // Which allows a better performance and needs to do less mocks
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(OrderController.class)
 public class OrderControllerIntegrationTests {
         @Autowired
         private MockMvc mockMvc;
