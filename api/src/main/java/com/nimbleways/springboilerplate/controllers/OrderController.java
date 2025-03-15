4c12b3d94ee2b804f67db354e4f0278c2a657512
Ammar HOMSI <ahomsi@pca.ma>
2025-03-15 11:17:04 +0000

    fix: move business logic to service

--- api/src/main/java/com/nimbleways/springboilerplate/controllers/OrderController.java
+++ api/src/main/java/com/nimbleways/springboilerplate/controllers/OrderController.java
@@ -1,17 +1,7 @@
 package com.nimbleways.springboilerplate.controllers;
 
 import com.nimbleways.springboilerplate.dto.product.ProcessOrderResponse;
-import com.nimbleways.springboilerplate.entities.Order;
-import com.nimbleways.springboilerplate.entities.Product;
-import com.nimbleways.springboilerplate.entities.ProductType;
-import com.nimbleways.springboilerplate.repositories.OrderRepository;
-import com.nimbleways.springboilerplate.repositories.ProductRepository;
 import com.nimbleways.springboilerplate.services.implementations.ProductService;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,52 +19,10 @@ public class OrderController {
     @Autowired
     private ProductService ps;
 
-    @Autowired
-    private ProductRepository pr;
-
-    @Autowired
-    private OrderRepository or;
-
     @PostMapping("{orderId}/processOrder")
     @ResponseStatus(HttpStatus.OK)
     public ProcessOrderResponse processOrder(@PathVariable Long orderId) {
-        Order order = or.findById(orderId).get();
-        log.info("order={}", order);
-        Set<Product> products = order.getItems();
-        for (Product p : products) {
-            switch (p.getType()) {
-                case NORMAL -> {
-                    if (p.getAvailable() > 0) {
-                        p.setAvailable(p.getAvailable() - 1);
-                        pr.save(p);
-                    } else {
-                        int leadTime = p.getLeadTime();
-                        if (leadTime > 0) {
-                            ps.notifyDelay(leadTime, p);
-                        }
-                    }
-                }
-                case SEASONAL -> {
-                    // Add new season rules
-                    if ((LocalDate.now().isAfter(p.getSeasonStartDate()) && LocalDate.now().isBefore(p.getSeasonEndDate())
-                        && p.getAvailable() > 0)) {
-                        p.setAvailable(p.getAvailable() - 1);
-                        pr.save(p);
-                    } else {
-                        ps.handleSeasonalProduct(p);
-                    }
-                }
-                case EXPIRABLE -> {
-                    if (p.getAvailable() > 0 && p.getExpiryDate().isAfter(LocalDate.now())) {
-                        p.setAvailable(p.getAvailable() - 1);
-                        pr.save(p);
-                    } else {
-                        ps.handleExpiredProduct(p);
-                    }
-                }
-            }
-        }
-
-        return new ProcessOrderResponse(order.getId());
+        Long id = ps.processOrder(orderId);
+        return new ProcessOrderResponse(id);
     }
 }
--- api/src/main/java/com/nimbleways/springboilerplate/services/implementations/ProductService.java
+++ api/src/main/java/com/nimbleways/springboilerplate/services/implementations/ProductService.java
@@ -1,7 +1,11 @@
 package com.nimbleways.springboilerplate.services.implementations;
 
 import java.time.LocalDate;
+import java.util.Set;
 
+import com.nimbleways.springboilerplate.entities.Order;
+import com.nimbleways.springboilerplate.repositories.OrderRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +13,57 @@ import com.nimbleways.springboilerplate.entities.Product;
 import com.nimbleways.springboilerplate.repositories.ProductRepository;
 
 @Service
+@Slf4j
 public class ProductService {
 
     @Autowired
     ProductRepository pr;
 
     @Autowired
+    private OrderRepository or;
+
+    @Autowired
     NotificationService ns;
+
+    public Long processOrder(Long orderId) {
+        Order order = or.findById(orderId).get();
+        log.info("order={}", order);
+        Set<Product> products = order.getItems();
+        for (Product p : products) {
+            switch (p.getType()) {
+                case NORMAL -> {
+                    if (p.getAvailable() > 0) {
+                        p.setAvailable(p.getAvailable() - 1);
+                        pr.save(p);
+                    } else {
+                        int leadTime = p.getLeadTime();
+                        if (leadTime > 0) {
+                            notifyDelay(leadTime, p);
+                        }
+                    }
+                }
+                case SEASONAL -> {
+                    // Add new season rules
+                    if ((LocalDate.now().isAfter(p.getSeasonStartDate()) && LocalDate.now().isBefore(p.getSeasonEndDate())
+                        && p.getAvailable() > 0)) {
+                        p.setAvailable(p.getAvailable() - 1);
+                        pr.save(p);
+                    } else {
+                        handleSeasonalProduct(p);
+                    }
+                }
+                case EXPIRABLE -> {
+                    if (p.getAvailable() > 0 && p.getExpiryDate().isAfter(LocalDate.now())) {
+                        p.setAvailable(p.getAvailable() - 1);
+                        pr.save(p);
+                    } else {
+                        handleExpiredProduct(p);
+                    }
+                }
+            }
+        }
+        return order.getId();
+    }
 
     public void notifyDelay(int leadTime, Product p) {
         p.setLeadTime(leadTime);
